bf81fbb2e20cdc480896040f431881b3915bec15
Paul <caliguidpaul@gmail.com>
2022-10-02 08:32:04 +0800

    working version

--- script.js
+++ script.js
@@ -21,83 +21,114 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    image1.classList.remove('image-win');
+    image2.classList.remove('image-win');
+    image1.setAttribute('src',`${playerSelection}.png`);
+    image2.setAttribute('src',`${computerSelection}.png`);
+    roundText.textContent = `Round: ${roundNumber}`;
+    roundNumber++;;
+
     if (playerSelection === computerSelection) {
-        return `Draw! Both players select ${playerSelection}`;
+        image1.classList.add('image-win')
+        image2.classList.add('image-win')
+        roundNumber--;
+        detailText.textContent = `Draw! Both players select ${playerSelection}`;
     } else if (playerSelection == 'Rock') {
         if (computerSelection == 'Paper') {
-            computerScore+=1;
-            return `Computer wins round! ${computerSelection} beats ${playerSelection}`;
+            computerScore += 1;
+            image2.classList.add('image-win')
+            detailText.textContent = `Computer wins round! ${computerSelection} beats ${playerSelection}`;
         } else { //computerSelection === 'Scissors
-            playerScore+=1;
-            return `Player wins round! ${playerSelection} beats ${computerSelection}`;
+            playerScore += 1;
+            image1.classList.add('image-win')
+            detailText.textContent = `Player wins round! ${playerSelection} beats ${computerSelection}`;
         }
     } else if (playerSelection == 'Paper') {
         if (computerSelection == 'Scissors') {
-            computerScore+=1;
-            return `Computer wins round! ${computerSelection} beats ${playerSelection}`;
+            computerScore += 1;
+            image2.classList.add('image-win')
+            detailText.textContent = `Computer wins round! ${computerSelection} beats ${playerSelection}`;
         } else { //computerSelection === 'Rock
-            playerScore+=1;
-            return `Player wins round! ${playerSelection} beats ${computerSelection}`;
+            playerScore += 1;
+            image1.classList.add('image-win')
+            detailText.textContent = `Player wins round! ${playerSelection} beats ${computerSelection}`;
         }
     } else { //chooses Scissors
         if (computerSelection == 'Rock') {
-            computerScore+=1;
-            return `Computer wins round! ${computerSelection} beats ${playerSelection}`;
+            computerScore += 1;
+            image2.classList.add('image-win')
+            detailText.textContent = `Computer wins round! ${computerSelection} beats ${playerSelection}`;
         } else { //computerSelection === 'Paper'
-            playerScore+=1;
-            return `Player wins round! ${playerSelection} beats ${computerSelection}`;
+            playerScore += 1;
+            image1.classList.add('image-win')
+            detailText.textContent = `Player wins round! ${playerSelection} beats ${computerSelection}`;
         }
     }
+    ScoreText.textContent = `Player: ${playerScore} Computer:${computerScore}`
 }
 
 //Initial Declarations
 let playerScore = 0;
 let computerScore = 0;
+let roundNumber = 1;
+let playerSelection = "";
 
-let playerSelection;
+const roundText = document.querySelector('#round');
+const ScoreText = document.querySelector('#score');
+const image1 = document.querySelector('#image1');
+const image2 = document.querySelector('#image2')
+const detailText = document.querySelector('#detail')
 
+const buttonRock = document.querySelector('#rock');
+const buttonPaper = document.querySelector('#paper');
+const buttonScissors = document.querySelector('#scissors');
+const buttonReset = document.querySelector('#reset');
 
-function selectForPlayer() {
-    while (playerSelection !== 'Rock' && playerSelection !== 'Paper' && playerSelection !== 'Scissors') {
-        playerSelection = prompt("Pick your choice(Rock/Paper/Scissors)");
+buttonRock.addEventListener('click', (e) => {
+    playerSelection = 'Rock';
+    playRound(playerSelection, getComputerChoice());
+    checkWin()
+})
 
-        //Clean inputs
-        playerSelection = capitalizeFirstLetterOnly(playerSelection);
-        playerSelection = playerSelection.trim();
-    }
+buttonPaper.addEventListener('click', (e) => {
+    playerSelection = 'Paper';
+    playRound(playerSelection, getComputerChoice());
+    checkWin()
+})
+
+buttonScissors.addEventListener('click', (e) => {
+    playerSelection = 'Scissors';
+    playRound(playerSelection, getComputerChoice());
+    checkWin()
+})
+
+buttonReset.addEventListener('click', (e) => {
+    playerSelection = '';
+    roundNumber = 1;
+    playerScore = 0;
+    computerScore = 0;
+    image1.classList.remove('image-win');
+    image2.classList.remove('image-win');
+    image1.setAttribute('src',"Initial.png");
+    image2.setAttribute('src',"Initial.png");
+    roundText.textContent = "Round: ";
+    ScoreText.textContent = "Player: 0 Computer:0";
+    detailText.textContent = "Waiting for first move..";
+    buttonRock.disabled = false;
+    buttonPaper.disabled = false;
+    buttonScissors.disabled = false;
+})
+
+function checkWin() {
+    if (playerScore === 5){
+        detailText.textContent += ". Player wins game!";
+        buttonRock.disabled = true;
+        buttonPaper.disabled = true;
+        buttonScissors.disabled = true;
+    } else if (computerScore === 5){
+        detailText.textContent += ". Computer wins game!";
+        buttonRock.disabled = true;
+        buttonPaper.disabled = true;
+        buttonScissors.disabled = true;
+    }   
 }
-
-function game() {
-    for (let i = 0; i < 5; i++) { //5 Rounds
-
-        if(playerScore == 3 || computerScore == 3) { //First to 3 wins!
-            break; //break out of for loop
-        }
-
-        console.log(`Current round: ${i+1}, player_score: ${playerScore}, computer_score: ${computerScore}`)
-
-        selectForPlayer();
-
-        let result = playRound(playerSelection, getComputerChoice());
-        console.log(result);
-
-        if (result.slice(0,4) === 'Draw') { // If result is draw, subtract 1 to i to repeat round.
-            i--;
-        } 
-
-        playerSelection=""; //reset playerSelection.
-    }
-
-    if (playerScore > computerScore) {
-        console.log("Player wins 5 round game!");
-        console.log(`player_score: ${playerScore}, computer_score: ${computerScore}`)
-    } 
-    
-    else {
-        console.log("Computer wins 5 round game!");
-        console.log(`player_score: ${playerScore}, computer_score: ${computerScore}`)
-    }
-}
-
-//Start game!
-game();
